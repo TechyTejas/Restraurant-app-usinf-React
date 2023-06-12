829c4caa30f1f99a869bcf7d4f8c5493d22992a5
tejas <sadadetej.27@gmail.com>
2023-06-12 13:55:37 +0530

    Summary added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {Fragment} from 'react';
+import Meals from './componenets/Meals/Meals';
 import Header from'./componenets/Layouts/Header';
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
--- src/componenets/Layouts/Header.js
+++ src/componenets/Layouts/Header.js
@@ -6,7 +6,7 @@ import HeaderCartbutton from './HeaderCartbutton';
 const  header =props=> {
   return  <React.Fragment>
          <header className={classes.header}>
-            <h1>ReactMeals</h1>
+            <h1>Restaurants App</h1>
             <HeaderCartbutton/>
          </header>
          <div className={classes['main-image']}>
