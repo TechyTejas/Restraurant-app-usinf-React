b09bbf84ebfe4b450738b678cdcffded800853a6
tejas <sadadetej.27@gmail.com>
2023-06-12 11:51:22 +0530

    Header Created

---
+++ src/componenets/Layouts/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import mealsImage from '../../assets/meals.jpg'
+import classes from './header.module.css';
+import HeaderCartbutton from './HeaderCartbutton';
+
+const  header =props=> {
+  return  <React.Fragment>
+         <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartbutton/>
+         </header>
+         <div className={classes['main-image']}>
+            <img src={mealsImage} alt="a table full of delicious food!"/>
+         </div>
+
+    </React.Fragment>
+  
+}
+
+
+export default header;
